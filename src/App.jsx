fca0f6fcee4f9b41503419c29b208204a328444a
shashanksingh0617 <shashank.singh.0617@gmail.com>
2023-04-24 09:09:19 +0530

    Intitial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
-import Home from './components/Home';
-import Contact from './components/Contact';
-import About from './components/About';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-import "./styles/App.css"
-import "./styles/Header.css"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './components/Header';
-import Product from './components/Product';
+import './styles/App.scss'
 const App = () => {
   return (
     <Router>
     <Header/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/product/:id' element={<Product/>}/>
-        <Route path='*' element={<p>Page Not Found 404</p>}/>
       </Routes>
     </Router>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-    </nav>
-    )
+    <div>
+        <h1>Hello</h1>
+
+    </div>
+  )
 }
 
 export default Header
